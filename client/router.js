6cc7db2861c189ed486d065167ad83e910975c54
franpham <franpham@gmail.com>
2015-10-07 18:23:54 -1000

    got login/ logout and redirects working

---
+++ client/router.js
@@ -0,0 +1,28 @@
+"use strict";
+
+Router.configure({
+  layoutTemplate : 'AppLayout',   // default layout always rendered;
+  before : function () {
+    if (!Meteor.user()) {
+      if (Meteor.loggingIn()) {
+        // do nothing, just call this.next();
+        this.next();
+      }
+      else {
+        this.render('Login');
+      }
+    }
+    else {
+      this.redirect('/Dashboard');
+    }
+  },
+  after : function () {
+    if (!Meteor.user()) {
+      this.redirect('/Login');
+    }
+  }
+});
+
+// By default the router will render the capitalized name of the template, with punctuations removed.
+Router.route('/login');
+Router.route('/dashboard');
